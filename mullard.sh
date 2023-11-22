6163a07b117dea988437bd28fc48da86cb8aebb4
ip-rw <ed@pearson.co.uk>
2023-11-22 02:20:07 +0000

    Update mullard.sh

--- mullard.sh
+++ mullard.sh
@@ -11,7 +11,7 @@ Usage: $0 [MullvadID]
 
 Description:
 Here be very functional dragons. Give it a Mullvad ID and you'll end up with a nice proxy pool.
-It will then download all the WireGuard configs, make a few tweaks, find the fastest one based on ping,
+It will download all the WireGuard configs, make a few tweaks, find the fastest one based on ping,
 connect (requires wg-quick) but without using AllowedIPs 0/0 so your internet will continue to function.
 It'll then run a rotating proxy server that makes use of Mullvad's unique and rather wonderful SOCKS5 facility.
 
